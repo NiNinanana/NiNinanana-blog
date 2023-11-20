985ef9103e5bcbdcc0580a2570c0dc6020ef971b
NiNinanana <yeen00123@gmail.com>
2023-11-20 21:50:35 +0900

    react-query 설치 후 셋팅

--- client/apps/admin/src/app/layout.tsx
+++ client/apps/admin/src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+
+import Layout from "components/layout";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -12,7 +14,9 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Layout>{children}</Layout>
+      </body>
     </html>
   );
 }

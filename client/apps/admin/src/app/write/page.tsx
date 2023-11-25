e585b51588e3f3ad4ef53ed29bf09f071410b240
NiNinanana <yeen00123@gmail.com>
2023-11-25 11:48:52 +0900

    write page에서 에디터를 가져다가 사용할 수 있게 추가

--- client/apps/admin/src/app/write/page.tsx
+++ client/apps/admin/src/app/write/page.tsx
@@ -5,13 +5,15 @@ import { useForm } from "react-hook-form";
 
 import { usePostBlog } from "queries/blogs";
 import { Blog } from "types/blogs";
+import Editor from "components/editor";
 
 const WritePage = () => {
-  const { handleSubmit, register } = useForm<Blog>();
+  const { handleSubmit, register, setValue } = useForm<Blog>();
 
   const { mutate } = usePostBlog();
 
   const onSubmit = (data: Blog) => {
+    if (!confirm("생성?")) return;
     mutate(data);
   };
 
@@ -21,16 +23,15 @@ const WritePage = () => {
       <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-3">
         <div className="flex gap-5">
           <span>Title</span>
-          <input className="text-black" {...register("title")} />
+          <input className="text-black border" {...register("title")} />
         </div>
         <div className="flex gap-5">
           <span>Category</span>
-          <input className="text-black" {...register("category")} />
+          <input className="text-black border" {...register("category")} />
         </div>
-        <div className="flex gap-5">
-          <span>Contents</span>
-          <textarea className="text-black" {...register("contents")} />
-        </div>
+        <br />
+        <span>Contents</span>
+        <Editor setValue={setValue} />
         <button className="border w-[70px]">생성</button>
       </form>
     </div>

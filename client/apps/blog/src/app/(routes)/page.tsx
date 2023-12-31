2fc9fa9138cb96d5ec64855ae5033fe386cfae9e
NiNinanana <yeen00123@gmail.com>
2023-12-31 12:12:17 +0900

    기본 페이지 만들어 기본 리스트 생성

--- client/apps/blog/src/app/(routes)/page.tsx
+++ client/apps/blog/src/app/(routes)/page.tsx
@@ -1,16 +1,34 @@
+"use client";
+
 import React from "react";
 
+import ArticleCard from "components/card";
 import Bubble from "components/Home/bubble";
+import { useGetBlogs } from "queries/blogs";
 
 export default function Home() {
+  const { data } = useGetBlogs();
+
   return (
-    <div>
+    <div className="flex flex-col gap-10">
       <div className="flex justify-between">
         <Bubble href="/html-css" imageName="html.svg" />
         <Bubble href="/js-ts" imageName="javascript.svg" />
         <Bubble href="/react-next" imageName="react.png" />
         <Bubble href="/express" imageName="express.png" />
         <Bubble href="/cs" imageName="computer-science.png" isNeedRadius={true} />
+      </div>
+      <div className="flex flex-col gap-10">
+        {data?.map((blog) => (
+          <ArticleCard
+            imageUrl={blog?.imageUrl}
+            key={blog.id}
+            category={blog.category}
+            date={blog.updatedAt?.toDate().toString()}
+            summary={blog?.summary}
+            title={blog.title}
+          />
+        ))}
       </div>
     </div>
   );

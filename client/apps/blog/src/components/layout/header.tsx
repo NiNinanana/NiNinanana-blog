f18c4a785e9bf509f859d07ac4436a61ba44dfc9
NiNinanana <yeen00123@gmail.com>
2023-12-31 12:04:10 +0900

    header에 z index 추가 및 로고에 링크 추가

--- client/apps/blog/src/components/layout/header.tsx
+++ client/apps/blog/src/components/layout/header.tsx
@@ -3,9 +3,11 @@ import React from "react";
 
 const Header = () => {
   return (
-    <header className="h-20 shadow-md flex justify-center items-center sticky -top-4 bg-white pt-4">
+    <header className="h-20 shadow-md flex justify-center items-center sticky -top-4 bg-white pt-4 z-10">
       <div className="max-w-[1100px] px-10 w-full flex items-center justify-between">
-        <h1 className="text-xl">NiNinanana</h1>
+        <Link href="/" className="text-xl">
+          NiNinanana
+        </Link>
         <div className="flex gap-7">
           <Link href="/html-css">HTML / CSS</Link>
           <Link href="/js-ts">JS / TS</Link>

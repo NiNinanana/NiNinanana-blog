0694803c1add9aa0c6c24f9b0495f4c651036740
NiNinanana <yeen00123@gmail.com>
2024-01-05 22:30:48 +0900

    blog 가져오는 쿼리 지정

--- client/apps/blog/src/apis/blog.ts
+++ client/apps/blog/src/apis/blog.ts
@@ -1,9 +1,15 @@
-import { getDocs, Query, query } from "firebase/firestore";
+import { doc, getDoc, getDocs, Query, query } from "firebase/firestore";
 
 import { Article } from "types/article";
-import { blogsFirestore } from "utils/firebase/store";
+import { blogsFirestore, firebaseFirestore } from "utils/firebase/store";
 
 export const getBlogs = async () => {
   const snapshot = await getDocs(query(blogsFirestore) as Query<Article, Article>);
   return snapshot;
+};
+
+export const getBlog = async (id: string) => {
+  const docRef = doc(firebaseFirestore, "blogs", id);
+  const docSnap = await getDoc(docRef);
+  return docSnap;
 };

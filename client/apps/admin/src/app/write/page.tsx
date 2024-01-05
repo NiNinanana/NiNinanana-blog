b0f9ad6c70e684713d099e19475b6f544d9494c7
강선우 <74632731+NiNinanana@users.noreply.github.com>
2024-01-05 21:30:16 +0900

    Client/admin/editor (#9)
    * 기본 레이아웃 설정
    
    * editor 새로 추가 및 기본 스타일 적용
    
    * react-markdown 라이브러리 설치 및 기존 에디터 관련 라이브러리 삭제

--- client/apps/admin/src/app/write/page.tsx
+++ client/apps/admin/src/app/write/page.tsx
@@ -6,32 +6,44 @@ import { useForm } from "react-hook-form";
 import { usePostBlog } from "queries/blog";
 import { Blog } from "types/blogs";
 import Editor from "components/editor";
+import { useRouter } from "next/navigation";
 
 const WritePage = () => {
-  const { handleSubmit, register, setValue } = useForm<Blog>();
+  const { push } = useRouter();
+  const { handleSubmit, register, watch } = useForm<Blog>();
+  const contents = watch().contents;
 
   const { mutate } = usePostBlog();
 
   const onSubmit = (data: Blog) => {
     if (!confirm("생성?")) return;
-    mutate(data);
+    mutate(data, {
+      onSuccess: () => {
+        alert("완료");
+        push("/");
+      },
+      onError: (err) => {
+        alert("error 발생");
+        console.error(err);
+      },
+    });
   };
 
   return (
-    <div className="p-5">
+    <div>
       <div className="mb-16">WritePage</div>
       <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-3">
-        <div className="flex gap-5">
+        <div className="flex items-center gap-5">
           <span>Title</span>
-          <input className="text-black border" {...register("title")} />
+          <input className="text-black border py-2 pl-2" {...(register("title"), { required: true })} />
         </div>
-        <div className="flex gap-5">
+        <div className="flex items-center gap-5">
           <span>Category</span>
-          <input className="text-black border" {...register("category")} />
+          <input className="text-black border py-2 pl-2" {...(register("category"), { required: true })} />
         </div>
         <br />
         <span>Contents</span>
-        <Editor setValue={setValue} />
+        <Editor register={register("contents")} contents={contents} />
         <button className="border w-[70px]">생성</button>
       </form>
     </div>
--- client/apps/admin/src/components/layout/index.tsx
+++ client/apps/admin/src/components/layout/index.tsx
@@ -1,13 +1,21 @@
 import React, { ReactNode } from "react";
 
 import QueryProvider from "queries/provider";
+import Header from "./header";
 
 interface LayoutProps {
   children: ReactNode;
 }
 
 const Layout = ({ children }: LayoutProps) => {
-  return <QueryProvider>{children}</QueryProvider>;
+  return (
+    <QueryProvider>
+      <Header />
+      <main className="flex justify-center">
+        <div className="max-w-[1100px] w-full px-10 py-5">{children}</div>
+      </main>
+    </QueryProvider>
+  );
 };
 
 export default Layout;
